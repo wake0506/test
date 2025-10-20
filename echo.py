f84586255fd6abc7572f88cc7f8455ef7eb831fa
Young <1292602364@qq.com>
2025-10-20 19:09:15 +0800

    refactor: tweak prompt on main

--- echo.py
+++ echo.py
@@ -2,7 +2,7 @@ import sys
 
 def echo():
     shout = "\-s" in sys.argv
-    message = input("Enter something: ")
+    message = input("Say something: ")
     print(message.upper() if shout else message)
 
 if __name__ == "__main__":
